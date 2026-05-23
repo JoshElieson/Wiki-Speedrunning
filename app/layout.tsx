0f2f0ec7a10a7f58c75144044e4340e6dc197b5e
Josh <Josh@DESKTOP-5N206V5>
2026-05-22 21:27:51 -0600

    bug fix hero page

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,10 +35,8 @@ export default async function RootLayout({
     >
       <body className="min-h-full bg-[var(--background)] text-[var(--foreground)]">
         <AppProviders session={session}>
-          <div className="flex h-dvh flex-col overflow-hidden">
-            <TopNav />
-            <div className="min-h-0 flex-1 overflow-y-auto">{children}</div>
-          </div>
+          <TopNav />
+          {children}
         </AppProviders>
       </body>
     </html>
